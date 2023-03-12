9993a4ff995b8d155be54b2d5d6aec63966352f5
Joeyyy09 <imt_2021063@iiitm.ac.in>
2023-03-12 23:42:16 +0530

    Added Landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Nav from "./Components/Navbar";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav></Nav>
+
+      <main className="bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-pink-800 via-black to-black min-h-screen flex flex-col justify-center items-center">
+        <div className="">
+          <div className="text-white ">
+            <h1 className="text-6xl font-medium">The Simple Way</h1>
+            <h3 className="text-3xl mt-2">to store your Passwords</h3>
+            <p className="mt-8">
+              Keep all of your login information safe and secure <br /> with our
+              state-of-the-art password manager.
+            </p>
+            <div className="mt-8">
+              <a
+                href="javascript:void(0)"
+                className="px-4 py-2 text-sm text-gray-800 bg-white rounded-3xl shadow hover:bg-gray-100"
+              >
+                Get Started
+              </a>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
